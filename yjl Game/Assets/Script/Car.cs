1f1e17a0d15d84279cc3914fe8a7c71167753655
KIM <yjl1845@naver.com>
2023-09-01 21:17:34 +0900

    2023.09.01 유니티 깃허브 연동

---
+++ yjl Game/Assets/Script/Car.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car: MonoBehaviour
+{
+    public float speed = 20.0f;
+
+    protected int carType = 0;
+
+    private string carName;
+}
