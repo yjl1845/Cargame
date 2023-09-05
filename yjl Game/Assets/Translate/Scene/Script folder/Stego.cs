1162bc53c907603d143bb64e7b2a9e8c6fcba188
yjl1845 <yjl1845@naver.com>
2023-09-06 00:18:17 +0900

    회전

--- yjl Game/Assets/Translate/Scene/Script folder/Stego.cs
+++ yjl Game/Assets/Translate/Scene/Script folder/Stego.cs
@@ -9,6 +9,8 @@ public class Stego : MonoBehaviour
 
     public Vector3 direction;
     public float speed = 1.0f;
+    public float rotateSpeed = 0.1f;
+
 
     // Start is called before the first frame update
     void Start()
@@ -20,30 +22,30 @@ public class Stego : MonoBehaviour
     void Update()
     {
         #region Input.GetKeyDown
-         // if (Input.GetKeyDown(KeyCode.W))
-         // {
-         //     Debug.Log("스테고 걷기");
-         // }
-         // 
-         // if (Input.GetKeyDown(KeyCode.LeftArrow))
-         // {
-         //     transform.position += new Vector3(-1,0,0);
-         // }
-         // 
-         // if (Input.GetKeyDown(KeyCode.RightArrow))
-         // {
-         //     transform.position += new Vector3(1, 0, 0);
-         // }
-         // 
-         // if (Input.GetKeyDown(KeyCode.UpArrow))
-         // {
-         //     transform.position += new Vector3(0, 0, 1);
-         // }
-         // 
-         // if (Input.GetKeyDown(KeyCode.DownArrow))
-         // {
-         //     transform.position += new Vector3(0, 0, -1);
-         // }
+        // if (Input.GetKeyDown(KeyCode.W))
+        // {
+        //     Debug.Log("스테고 걷기");
+        // }
+        // 
+        // if (Input.GetKeyDown(KeyCode.LeftArrow))
+        // {
+        //     transform.position += new Vector3(-1,0,0);
+        // }
+        // 
+        // if (Input.GetKeyDown(KeyCode.RightArrow))
+        // {
+        //     transform.position += new Vector3(1, 0, 0);
+        // }
+        // 
+        // if (Input.GetKeyDown(KeyCode.UpArrow))
+        // {
+        //     transform.position += new Vector3(0, 0, 1);
+        // }
+        // 
+        // if (Input.GetKeyDown(KeyCode.DownArrow))
+        // {
+        //     transform.position += new Vector3(0, 0, -1);
+        // }
         #endregion
 
         #region
@@ -51,14 +53,23 @@ public class Stego : MonoBehaviour
         // Input.GetAxis : -1 ~ 1사이의 값을 반환하는 함수
         direction.x = Input.GetAxis("Horizontal");
         direction.z = Input.GetAxis("Vertical");
+        Vector3 dir = new Vector3(direction.x, 0, direction.z);
 
         // v = v0 + vt
         // Time.deltatime : 전 프레임이 완료되기까지 걸린 시간
 
         direction.Normalize();
 
-        transform.Translate(direction*speed*Time.deltaTime);
+        transform.Translate(direction * speed * Time.deltaTime);
 
-        #endregion
+        if (!(direction.x == 0 && direction.y == 0))
+        {
+            // 이동과 회전을 함께 처리
+            transform.position += dir * speed * Time.deltaTime;
+            // 회전하는 부분. Point 1.
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(dir), Time.deltaTime * rotateSpeed);
+
+            #endregion
+        }
     }
 }
